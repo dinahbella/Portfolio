aea2831e9f419a4521bd4c43bd298fc84dd8d6bb
Dinah Blessing <gocitymobileapp1@gmail.com>
2025-03-13 10:50:50 +0100

    spinner

---
+++ Backend/components/Spinner.js
@@ -0,0 +1,9 @@
+import { DotLoader, MoonLoader, PacmanLoader } from "react-spinners";
+
+export default function Spinner() {
+  return (
+    <>
+      <MoonLoader color="#36d7b7" />
+    </>
+  );
+}
