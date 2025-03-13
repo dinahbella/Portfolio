2e82262a54ca9167e58a0be672472385ccbe0551
Dinah Blessing <gocitymobileapp1@gmail.com>
2025-03-13 10:40:42 +0100

    Project and product

---
+++ Backend/components/Project.js
@@ -0,0 +1,14 @@
+import ReactMarkdown from "react-markdown";
+import MarkdownEditor from "react-markdown-editor-lite";
+import "react-markdown-editor-lite/lib/index.css";
+import Head from "next/head";
+
+export default function Photo() {
+  return (
+    <>
+      <Head>
+        <title>Add Project</title>
+      </Head>
+    </>
+  );
+}
