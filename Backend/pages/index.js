93bf82077d26e09e239aa070c7778946e62f724c
Dinah Blessing <gocitymobileapp1@gmail.com>
2025-03-13 11:36:49 +0100

    dependency

---
+++ Backend/pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import React from "react";
+import Loading from "../components/Loading";
+import { Bar } from "react-chartjs-2";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Portfolio Backend</title>
+        <meta name="description" content="Blog website backend" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+    </div>
+  );
+}
