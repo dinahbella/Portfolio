7b9b05027918316d0980a0c46ba1643256997583
Dinah Blessing <gocitymobileapp1@gmail.com>
2025-03-20 13:47:54 +0100

    Phototable

---
+++ src/pages/photos/addphoto.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function addphoto() {
+  return <div>addphoto</div>;
+}
---
+++ src/pages/photos/allphotos.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function allphotos() {
+  return <div>allphotos</div>;
+}
