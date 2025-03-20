cd74fd9c5486858746e43d396ac096fbb302b454
Dinah Blessing <gocitymobileapp1@gmail.com>
2025-03-20 13:45:38 +0100

    project

--- src/pages/projects/allprojects.js
+++ src/pages/projects/allprojects.js
@@ -1,7 +1,7 @@
 import { BlogTable } from "@/components/BlogTable";
 import { ProjectTable } from "@/components/ProjectTable";
 import React from "react";
-import { FaFirstdraft } from "react-icons/fa";
+import { BsPostcardFill } from "react-icons/bs";
 
 export default function draftproject() {
   return (
@@ -9,14 +9,14 @@ export default function draftproject() {
       <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center p-3 gap-3 sm:gap-0">
         {/* Title */}
         <h2 className="text-xl sm:text-2xl text-blue-600 font-semibold">
-          Draft Projects
+          All Published Projects
         </h2>
 
         {/* Breadcrumb */}
         <div className="text-blue-600 flex items-center gap-2">
-          <FaFirstdraft className="text-lg sm:text-xl text-blue-600" />
+          <BsPostcardFill className="text-lg sm:text-xl text-blue-600" />
           <span>/</span>
-          <span>Draft Projects</span>
+          <span>All Projects</span>
         </div>
       </div>
       <ProjectTable />
