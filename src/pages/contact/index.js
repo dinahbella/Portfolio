cf7984cc2ff1c991f7e59d6e0efd820a24899122
Dinah Blessing <gocitymobileapp1@gmail.com>
2025-03-20 14:11:24 +0100

    Index

---
+++ src/pages/contact/index.js
@@ -0,0 +1,10 @@
+import { ConTable } from "@/components/ConTable";
+import React from "react";
+
+export default function index() {
+  return (
+    <div>
+      <ConTable />
+    </div>
+  );
+}
--- src/pages/photos/allphotos.js
+++ src/pages/photos/allphotos.js
@@ -1,5 +1,23 @@
+import { PhotoTable } from "@/components/PhotoTable";
 import React from "react";
+import { IoImagesSharp } from "react-icons/io5";
 
 export default function allphotos() {
-  return <div>allphotos</div>;
+  return (
+    <div>
+      <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center p-3 gap-3 sm:gap-0">
+        {/* Title */}
+        <h2 className="text-xl sm:text-2xl text-blue-600 font-semibold">
+          All Photos
+        </h2>
+        {/* Breadcrumb */}
+        <div className="text-blue-600 flex items-center gap-2">
+          <IoImagesSharp className="text-lg sm:text-xl text-blue-600" />
+          <span>/</span>
+          <span>All Photos</span>
+        </div>
+      </div>
+      <PhotoTable />
+    </div>
+  );
 }
