62104883ea930a7cc09b1bf7661a615fbc4ec3f7
Dinah Blessing <gocitymobileapp1@gmail.com>
2025-03-24 14:11:59 +0100

    Num.jsx

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -8,6 +8,7 @@ import { useEffect, useState } from "react";
 import Experience from "@/components/Experience";
 import Skills from "@/components/Skills";
 import Services from "@/components/Services";
+import Num from "@/components/Num";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -119,46 +120,7 @@ export default function Home() {
           </div>
         </motion.div>
       </div>
-      <div className="p-4 mt-10 w-full flex flex-col items-center hover:scale-x-110 duration-500">
-        <motion.div
-          initial={{ opacity: 0, y: -20 }}
-          animate={{ opacity: 1, y: 5 }}
-          transition={{ duration: 1.5, ease: "easeIn" }}
-          className="w-full max-w-5xl grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-6 text-center"
-        >
-          {/* Year of Experience */}
-          <div className="flex flex-col items-center gap-1">
-            <h2 className="font-bold text-3xl md:text-4xl">2 +</h2>
-            <p className="text-sm md:text-base">
-              Years of <br /> Experience
-            </p>
-          </div>
-
-          {/* Projects Completed */}
-          <div className="flex flex-col items-center gap-1 ">
-            <h2 className="font-bold text-3xl md:text-4xl">20 +</h2>
-            <p className="text-sm md:text-base">
-              Projects <br /> Completed
-            </p>
-          </div>
-
-          {/* Open Library */}
-          <div className="flex flex-col items-center gap-1">
-            <h2 className="font-bold text-3xl md:text-4xl">12 +</h2>
-            <p className="text-sm md:text-base">
-              Open <br /> Library
-            </p>
-          </div>
-
-          {/* Happy Customers */}
-          <div className="flex flex-col items-center gap-1">
-            <h2 className="font-bold text-3xl md:text-4xl">20 +</h2>
-            <p className="text-sm md:text-base">
-              Happy <br /> Customers
-            </p>
-          </div>
-        </motion.div>
-      </div>
+      <Num />
       <Services />
       <div className="p-5">
         <motion.div
