12465d418212b8bbe1c7dd96278e0641fad44b81
Dinah Blessing <gocitymobileapp1@gmail.com>
2025-04-23 16:26:19 +0100

    app

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -15,6 +15,10 @@ export default function App({ Component, pageProps }) {
       disableTransitionOnChange
     >
       <div className="flex flex-col min-h-screen">
+        <div className="flex flex-col bg-purple-700 text-white text-center py-4">
+          Close Your eyes This website is still under construction. Please check
+          back later.
+        </div>
         <main className="flex-grow">
           <ToastContainer
             position="top-right"
