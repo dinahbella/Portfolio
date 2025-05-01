5a9e115bff0962a24c017ce22c27daba7e2a8e6b
Dinah Blessing <gocitymobileapp1@gmail.com>
2025-05-01 15:30:11 +0100

    office

--- inkvision/pages/contacts/index.js
+++ inkvision/pages/contacts/index.js
@@ -247,28 +247,6 @@ const Contact = () => {
                   ))}
                 </div>
               </div>
-
-              <div className="bg-white dark:bg-gray-800 rounded-2xl shadow-lg p-8">
-                <h3 className="text-xl font-bold text-gray-900 dark:text-white mb-4">
-                  Our Office
-                </h3>
-                <div className="aspect-w-16 aspect-h-9 rounded-xl overflow-hidden">
-                  <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.215256018603!2d-73.98784492452563!3d40.74844097138962!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c259a9b3117469%3A0xd134e199a405a163!2sEmpire%20State%20Building!5e0!3m2!1sen!2sus!4v1711234567890!5m2!1sen!2sus"
-                    width="100%"
-                    height="300"
-                    style={{ border: 0 }}
-                    allowFullScreen
-                    loading="lazy"
-                    className="rounded-xl"
-                  ></iframe>
-                </div>
-                <p className="mt-4 text-gray-600 dark:text-gray-300">
-                  123 Business Ave, Suite 500
-                  <br />
-                  New York, NY 10001
-                </p>
-              </div>
             </motion.div>
 
             {/* Contact Form */}
