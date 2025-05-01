74de858fb9442ecbd5afe29cd23ffd89efdb538b
Dinah Blessing <gocitymobileapp1@gmail.com>
2025-05-01 15:23:52 +0100

    png

--- inkvision/pages/_document.js
+++ inkvision/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/Inkvision2.svg" type="image/png" />
+        <link rel="icon" href="/Inkvision2.png" type="image/png" />
       </Head>
       <body className="antialiased">
         <Main />
