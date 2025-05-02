6253fdaa68e6f38b087bce90c4ddb6dd9d86e710
Dinah Blessing <gocitymobileapp1@gmail.com>
2025-05-02 14:00:49 +0100

    photos

--- inkvision/pages/photos/index.js
+++ inkvision/pages/photos/index.js
@@ -224,7 +224,7 @@ export default function PhotoGallery() {
           ) : (
             <motion.div
               variants={containerVariants}
-              className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8"
+              className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8"
             >
               {alldata.map((photo) => (
                 <motion.div
